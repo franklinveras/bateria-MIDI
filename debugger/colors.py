80aed54aff522ce2e65e4967cfbd847f9168a7ee
Franklin Véras Sertão <franklinverass@gmail.com>
2016-12-24 22:00:46 -0200

    Submissão inicial.

---
+++ debugger/colors.py
@@ -0,0 +1,58 @@
+class bcolors:
+	NOCOLOR     = '\033[0m'
+	BOLD        = '\033[1m'
+	UNDERLINE   = '\033[4m'
+
+	HEADER      = '\033[95m'
+	OKBLUE      = '\033[94m'
+	OKGREEN     = '\033[92m'
+	WARNING     = '\033[93m'
+	FAIL        = '\033[91m'
+	
+	BLACK       = '\033[030m'
+	DARKGRAY    = '\033[1;30m'
+	LIGHTGRAY   = '\033[0;37m'
+	WHITE       = '\033[1;37m'
+	
+	RED         = '\033[0;31m'
+	LIGHTRED    = '\033[1;31m'
+	
+	GREEN       = '\033[0;32m'
+	LIGHTGREEN  = '\033[1;32m'
+	
+	BLUE        = '\033[0;34m'
+	LIGHTBLUE   = '\033[1;34m'
+	
+	CYAN        = '\033[0;36m'
+	LIGHTCYAN   = '\033[1;36m'
+	
+	PURPLE      = '\033[0;35m'
+	LIGHTPURPLE = '\033[1;35m'
+	
+	YELLOW      = '\033[1;33m'
+	ORANGE      = '\033[0;33m'
+
+#print bcolors.NOCOLOR,'NOCOLOR',bcolors.NOCOLOR
+#print bcolors.BOLD,'BOLD',bcolors.NOCOLOR
+#print bcolors.UNDERLINE,'UNDERLINE',bcolors.NOCOLOR
+#print bcolors.HEADER,'HEADER',bcolors.NOCOLOR
+#print bcolors.OKBLUE,'OKBLUE',bcolors.NOCOLOR
+#print bcolors.OKGREEN,'OKGREEN',bcolors.NOCOLOR
+#print bcolors.WARNING,'WARNING',bcolors.NOCOLOR
+#print bcolors.FAIL,'FAIL',bcolors.NOCOLOR
+#print bcolors.BLACK,'BLACK',bcolors.NOCOLOR
+#print bcolors.DARKGRAY,'DARKGRAY',bcolors.NOCOLOR
+#print bcolors.LIGHTGRAY,'LIGHTGRAY',bcolors.NOCOLOR
+#print bcolors.WHITE,'WHITE',bcolors.NOCOLOR
+#print bcolors.RED,'RED',bcolors.NOCOLOR
+#print bcolors.LIGHTRED,'LIGHTRED',bcolors.NOCOLOR
+#print bcolors.GREEN,'GREEN',bcolors.NOCOLOR
+#print bcolors.LIGHTGREEN,'LIGHTGREEN',bcolors.NOCOLOR
+#print bcolors.BLUE,'BLUE',bcolors.NOCOLOR
+#print bcolors.LIGHTBLUE,'LIGHTBLUE',bcolors.NOCOLOR
+#print bcolors.CYAN,'CYAN',bcolors.NOCOLOR
+#print bcolors.LIGHTCYAN,'LIGHTCYAN',bcolors.NOCOLOR
+#print bcolors.PURPLE,'PURPLE',bcolors.NOCOLOR
+#print bcolors.LIGHTPURPLE,'LIGHTPURPLE',bcolors.NOCOLOR
+#print bcolors.YELLOW,'YELLOW',bcolors.NOCOLOR
+#print bcolors.ORANGE,'ORANGE',bcolors.NOCOLOR
---
+++ debugger/debugger.py
@@ -0,0 +1,35 @@
+# Tarefas:
+#	 Implementar as demais mensagens MIDI (atualmente apenas NOTE_ON)
+
+import serial
+
+from colors import bcolors
+
+R  = bcolors.RED
+G  = bcolors.GREEN
+NC = bcolors.NOCOLOR
+
+ser = serial.Serial('/dev/ttyACM2', 115200)
+
+
+def Parse (msg): 
+	
+	# Mensagem midi: mmmmcccc ppppppp 0vvvvvv
+	# onde: m = mensagem , c = canal, p = nota(pitch), v = velocity(intensidade)
+
+	canal = (ord(msg[0]) & 0b1111) + 1
+	mensagem = ord(msg[0]) >> 4
+	pitch = ord(msg[1])
+	velocidade = ord(msg[2])
+
+	if velocidade > 0:
+		print "MIDI ch.%02d Note %3i %s On%s , vel %3i" %(canal,  pitch, G, NC, velocidade)
+	else:
+		print "MIDI ch.%02d Note %3i %sOff%s" %(canal, pitch, R, NC,)
+
+
+while True:
+	mensagem = ''
+	for msg_part in xrange(3):
+		mensagem +=  ser.read()
+	Parse(mensagem)
